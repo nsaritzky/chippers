157fedd977a207fd8d784f167f9b4fffd425b589
nsaritzky <nsaritzky@gmail.com>
2023-01-11 10:47:35 -0800

    All opcodes implemented

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 mod font;
+mod keyboard;
 
 use bitvec::prelude::*;
+use rand::prelude::*;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::surface::Surface;
 use std::cell::RefCell;
 use std::cmp::min;
+use std::collections::HashSet;
 use std::fs::read;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 use crate::font::FONT;
+use crate::keyboard::hex_keypad;
 
 const _PIXEL_OFF_COLOR: Color = Color::BLACK;
 const _PIXEL_ON_COLOR: Color = Color::WHITE;
@@ -22,27 +26,44 @@ const EXEC_SPEED: u32 = 700;
 #[derive(PartialEq, Debug)]
 struct Emulator {
     display_buffer: RefCell<Vec<u8>>,
+    display_flag: bool,
     memory: Vec<u8>,
     stack: Vec<u16>,
     program_counter: RefCell<usize>,
     index: u16,
     registers: [u8; 16],
     super_chip: bool,
+    delay_timer: RefCell<u8>,
+    sound_timer: RefCell<u8>,
+    keys_down: HashSet<usize>,
 }
 
 impl Emulator {
     fn new(super_chip: bool) -> Self {
         let mut emu = Emulator {
             display_buffer: RefCell::new(vec![0; (DISPLAY_HEIGHT * DISPLAY_WIDTH / 8) as usize]),
+            display_flag: false,
             memory: vec![0; 4096],
             stack: Vec::new(),
             index: 0,
             program_counter: RefCell::new(0x200),
             registers: [0; 16],
+            keys_down: HashSet::new(),
+            delay_timer: RefCell::new(0),
+            sound_timer: RefCell::new(0),
             super_chip,
         };
         emu.memory.splice(0x50..0x9F, FONT);
         emu
+    }
+
+    fn decrement_timers(&self) {
+        if *self.delay_timer.borrow() > 0 {
+            *self.delay_timer.borrow_mut() -= 1;
+        }
+        if *self.sound_timer.borrow() > 0 {
+            *self.sound_timer.borrow_mut() -= 1;
+        }
     }
 
     fn get_pixel(&self, x: u8, y: u8) -> bool {
@@ -67,9 +88,15 @@ impl Emulator {
     }
 
     fn decode(&mut self, instruction: BitVec<u8, Msb0>) -> Result<(), String> {
-        let (x, y) = instruction.split_at(8);
+        let (r, l) = instruction.split_at(8);
         let nibbles: Vec<u8> = instruction.chunks(4).map(|x| x.load::<u8>()).collect();
-        let ((_a, b), (c, d)) = (x.split_at(4), y.split_at(4));
+        let ((_a, b), (c, d)) = (r.split_at(4), l.split_at(4));
+        let x: usize = b.load_be();
+        let y: usize = c.load_be();
+        let vx = self.registers[x];
+        let vy = self.registers[y];
+        let nn = instruction[8..].load_be::<u8>();
+        let nnn = instruction[4..].load_be::<u16>();
         // let u = (a & 0b11110000) >> 4;
         match nibbles[0] {
             0x0 => match instruction[4..].load_be::<u16>() {
@@ -80,74 +107,62 @@ impl Emulator {
                 0xE0 => self.clear_screen(),
                 _ => {}
             },
-            0x1 => {
-                let addr = instruction[4..].load_be::<u16>();
-                self.jump(addr)
-            }
-            0x2 => {
-                let addr = instruction[4..].load_be::<u16>();
-                self.jump_subroutine(addr)
-            }
+            0x1 => self.jump(nnn),
+            0x2 => self.jump_subroutine(nnn),
             0x3 => {
-                let n: u8 = instruction[8..].load_be();
-                if self.registers[b.load::<usize>()] == n {
-                    *self.program_counter.borrow_mut() += 2;
+                if vx == nn {
+                    self.skip()
                 }
             }
             0x4 => {
-                let n: u8 = instruction[8..].load_be();
-                if self.registers[b.load::<usize>()] != n {
-                    *self.program_counter.borrow_mut() += 2;
+                if vx != nn {
+                    self.skip()
                 }
             }
             0x5 => {
-                if self.registers[b.load::<usize>()] == self.registers[c.load::<usize>()] {
-                    *self.program_counter.borrow_mut() += 2;
+                if vx == vy {
+                    self.skip()
                 }
             }
             0x6 => {
-                let n: u8 = instruction[8..].load_be();
-                self.set_x(b.load_be(), n);
+                self.registers[x] = nn;
             }
             0x7 => {
-                let n: u8 = instruction[8..].load_be();
-                self.add_x(b.load_be(), n)
+                self.registers[x] = vx.wrapping_add(nn);
             }
             0x8 => {
-                let x = self.registers[b.load::<usize>()];
-                let y = self.registers[c.load::<usize>()];
-                self.registers[b.load::<usize>()] = match d.load::<usize>() {
-                    0 => y,
-                    1 => x | y,
-                    2 => x & y,
-                    3 => x ^ y,
+                self.registers[x] = match d.load::<usize>() {
+                    0 => vy,
+                    1 => vx | vy,
+                    2 => vx & vy,
+                    3 => vx ^ vy,
                     4 => {
-                        if x.checked_add(y) == None {
+                        if vx.checked_add(vy) == None {
                             self.registers[0xF] = 1
                         };
-                        x.wrapping_add(y)
+                        vx.wrapping_add(vy)
                     }
                     5 => {
                         self.registers[0xF] = 1;
-                        if x < y {
+                        if vx < vy {
                             self.registers[0xF] = 0;
                         };
-                        x.wrapping_sub(y)
+                        vx.wrapping_sub(vy)
                     }
                     6 => {
-                        self.registers[0xF] = if x.view_bits::<Msb0>()[7] { 1 } else { 0 };
-                        x >> 1
+                        self.registers[0xF] = if vx.view_bits::<Msb0>()[7] { 1 } else { 0 };
+                        vx >> 1
                     }
                     7 => {
                         self.registers[0xF] = 1;
-                        if y < x {
+                        if vy < vx {
                             self.registers[0xF] = 0
                         };
-                        y.wrapping_sub(x)
+                        vy.wrapping_sub(vx)
                     }
                     0xe => {
-                        self.registers[0xF] = if x.view_bits::<Msb0>()[0] { 1 } else { 0 };
-                        x << 1
+                        self.registers[0xF] = if vx.view_bits::<Msb0>()[0] { 1 } else { 0 };
+                        vx << 1
                     }
                     _ => {
                         return Err(format!(
@@ -158,54 +173,97 @@ impl Emulator {
                 }
             }
             0x9 => {
-                if self.registers[b.load::<usize>()] != self.registers[c.load::<usize>()] {
-                    *self.program_counter.borrow_mut() += 2;
+                if vx != vy {
+                    self.skip()
                 }
             }
-            0xA => {
-                let addr = instruction[4..].load_be::<u16>();
-                self.index = addr;
+            0xA => self.index = nnn,
+            0xB => {
+                if self.super_chip {
+                    self.jump(u16::from(nn + vx))
+                } else {
+                    self.jump(nnn + u16::from(self.registers[0]))
+                }
             }
+            0xC => self.registers[x] = random::<u8>() & nn,
             0xD => {
-                let x: u8 = self.registers[b.load::<usize>()] % 64;
-                let y: u8 = self.registers[c.load::<usize>()] % 32;
+                self.display_flag = true;
+                let x_coord: u8 = vx % 64;
+                let y_coord: u8 = vy % 32;
                 self.registers[0xF] = 0;
-                let n: u8 = d.load();
-                for j in y..min(y + n, u8::try_from(DISPLAY_HEIGHT).unwrap() - 1) {
-                    let m = usize::from(self.index) + usize::from(j - y);
+                let n: u8 = d.load_be();
+                for j in y_coord..min(y_coord + n, u8::try_from(DISPLAY_HEIGHT).unwrap()) {
+                    let m = usize::from(self.index) + usize::from(j - y_coord);
                     let row = self.memory[m..m + 1].view_bits::<Msb0>();
                     // println!("row {:x?}", row);
-                    for i in x..min(x + 8, u8::try_from(DISPLAY_WIDTH).unwrap() - 1) {
-                        if self.get_pixel(i, j) && row[usize::from(i - x)] {
+                    for i in x_coord..min(x_coord + 8, u8::try_from(DISPLAY_WIDTH).unwrap()) {
+                        if self.get_pixel(i, j) && row[usize::from(i - x_coord)] {
                             self.set_pixel(i, j, false);
                             self.registers[0xF] = 1;
-                        } else if !self.get_pixel(i, j) && row[usize::from(i - x)] {
+                        } else if !self.get_pixel(i, j) && row[usize::from(i - x_coord)] {
                             self.set_pixel(i, j, true);
                         }
                     }
                 }
                 // println!("display buffer dump: {:x?}", &self.display_buffer.borrow())
             }
-            0xF => match instruction[8..].load_be::<u8>() {
+            0xE => match nn {
+                0x9E => {
+                    if self.keys_down.contains(&usize::from(vx)) {
+                        self.skip()
+                    }
+                }
+                0xA1 => {
+                    if !self.keys_down.contains(&usize::from(vx)) {
+                        self.skip()
+                    }
+                }
+                _ => {
+                    return Err(format!(
+                        "invalid opcode {:x?}",
+                        instruction.load_be::<u16>()
+                    ))
+                }
+            },
+            0xF => match nn {
+                0x07 => {
+                    self.registers[x] = *self.delay_timer.borrow();
+                }
+                0x0A => {
+                    if self.keys_down.is_empty() {
+                        *self.program_counter.borrow_mut() -= 2;
+                    } else {
+                        self.registers[x] =
+                            u8::try_from(*self.keys_down.iter().nth(0).unwrap()).unwrap();
+                    }
+                }
+                0x15 => {
+                    *self.delay_timer.borrow_mut() = vx;
+                }
+                0x18 => {
+                    *self.sound_timer.borrow_mut() = vx;
+                }
+                0x1E => {
+                    self.index += u16::from(vx);
+                }
                 0x29 => {
-                    self.index = 0x50 + u16::from(self.registers[b.load_be::<usize>()]);
+                    self.index = 0x50 + u16::from(vx * 5);
                 }
                 0x33 => {
-                    let n = self.registers[b.load_be::<usize>()];
-                    let hundreds = n / 100;
-                    let tens = (n % 100) / 10;
-                    let ones = n % 10;
+                    let hundreds = vx / 100;
+                    let tens = (vx % 100) / 10;
+                    let ones = vx % 10;
                     self.memory[usize::from(self.index)] = hundreds;
                     self.memory[usize::from(self.index) + 1] = tens;
                     self.memory[usize::from(self.index) + 2] = ones;
                 }
                 0x55 => {
-                    for i in 0..b.load_be::<usize>() + 1 {
+                    for i in 0..x + 1 {
                         self.memory[usize::from(self.index) + i] = self.registers[i];
                     }
                 }
                 0x65 => {
-                    for i in 0..b.load_be::<usize>() + 1 {
+                    for i in 0..x + 1 {
                         self.registers[i] = self.memory[usize::from(self.index) + i];
                     }
                 }
@@ -236,20 +294,13 @@ impl Emulator {
     }
 
     fn jump_subroutine(&mut self, n: u16) {
-        println!("program counter: {:x?}", self.program_counter.borrow());
         self.stack
             .push(u16::try_from(*self.program_counter.borrow()).unwrap());
-        println!("jumping to {:x?} from {:x?}", n, self.stack.last().unwrap());
         *self.program_counter.borrow_mut() = usize::from(n);
     }
 
-    fn set_x(&mut self, x: u8, n: u8) {
-        self.registers[usize::from(x)] = n;
-    }
-
-    fn add_x(&mut self, x: u8, n: u8) {
-        let m = self.registers[usize::from(x)];
-        self.registers[usize::from(x)] = n.wrapping_add(m);
+    fn skip(&mut self) {
+        *self.program_counter.borrow_mut() += 2;
     }
 }
 
@@ -257,6 +308,8 @@ pub fn main() -> Result<(), String> {
     let mut emulator = Emulator::new(false);
     let path = std::env::args().nth(1).expect("no file given");
     let prog: Vec<u8> = read(path).map_err(|e| e.to_string())?;
+
+    let mut now = Instant::now();
 
     emulator.memory.splice(0x200..0x200 + prog.len(), prog);
 
@@ -300,6 +353,22 @@ pub fn main() -> Result<(), String> {
                     } => {
                         break 'running;
                     }
+                    Event::KeyDown {
+                        scancode: Some(scancode),
+                        ..
+                    } => {
+                        if let Some(val) = hex_keypad(scancode) {
+                            emulator.keys_down.insert(val);
+                        }
+                    }
+                    Event::KeyUp {
+                        scancode: Some(scancode),
+                        ..
+                    } => {
+                        if let Some(val) = hex_keypad(scancode) {
+                            emulator.keys_down.remove(&val);
+                        }
+                    }
                     // Event::KeyDown {
                     //     scancode: Some(Scancode::S),
                     //     ..
@@ -310,24 +379,33 @@ pub fn main() -> Result<(), String> {
         }
         let instruction = emulator.fetch();
         emulator.decode(instruction)?;
-        let mut window_surface = window.surface(&event_pump)?;
-        // window_surface.set_palette(&palette)?;
 
-        let mut expanded_buf = emulator.display_buffer.borrow_mut();
+        if emulator.display_flag {
+            let mut window_surface = window.surface(&event_pump)?;
+            // window_surface.set_palette(&palette)?;
 
-        let surface = Surface::from_data(
-            &mut expanded_buf,
-            DISPLAY_WIDTH.into(),
-            DISPLAY_HEIGHT.into(),
-            DISPLAY_WIDTH / 8,
-            sdl2::pixels::PixelFormatEnum::Index1MSB,
-        )?;
+            let mut expanded_buf = emulator.display_buffer.borrow_mut();
 
-        // The rest of the game loop goes here...
+            let surface = Surface::from_data(
+                &mut expanded_buf,
+                DISPLAY_WIDTH.into(),
+                DISPLAY_HEIGHT.into(),
+                DISPLAY_WIDTH / 8,
+                sdl2::pixels::PixelFormatEnum::Index1MSB,
+            )?;
 
-        let converted = surface.convert(&pixel_format)?;
-        converted.blit_scaled(None, &mut window_surface, None)?;
-        window_surface.finish()?;
+            // The rest of the game loop goes here...
+
+            let converted = surface.convert(&pixel_format)?;
+            converted.blit_scaled(None, &mut window_surface, None)?;
+            window_surface.finish()?;
+        }
+        emulator.display_flag = false;
+
+        if now.elapsed() > Duration::from_micros(1000000 / 60) {
+            emulator.decrement_timers();
+            now = Instant::now();
+        }
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / EXEC_SPEED));
     }
     Ok(())
